eeae5aa96e1074d045d2c8bec83c43a82d8b3a98
ksand97 <krisa18@uia.no>
2021-11-10 13:51:48 +0100

    Update

--- request_api.py
+++ request_api.py
@@ -5,14 +5,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#Setting shortname, period and interval
-ticker = 'TSLA'
-period1 = int(time.mktime(datetime.datetime(2020, 11, 1, 23, 59).timetuple()))
-period2 = int(time.mktime(datetime.datetime(2021, 11, 1, 23, 59).timetuple()))
+# Setter inn ønskene paramtere
+ticker = input("Shortname:" )
+#ticker = 'TSLA'
+print("Year: ")
+year = input()
+print("Mounth:" )
+mounth = input()
+print("Day: ")
+day = input()
+print("Hour: ")
+hour = input()
+print("Minutes: ")
+minutes = input()
+
+#period1 = int(time.mktime(datetime.datetime(2020, 11, 1, 23, 59).timetuple()))
+period1 = int(time.mktime(datetime.datetime(int(year), int(mounth), int(day), int(hour), int(minutes)).timetuple()))
+
+print("Year end: ")
+year_end = input()
+print("Mounth end:" )
+mounth_end = input()
+print("Day end: ")
+day_end = input()
+print("Hour end: ")
+hour_end = input()
+print("Minutes end: ")
+minutes_end = input()
+period2 = int(time.mktime(datetime.datetime(int(year_end), int(mounth_end), int(day_end), int(hour_end), int(minutes_end)).timetuple()))
+#period2 = int(time.mktime(datetime.datetime(2021, 11, 1, 23, 59).timetuple()))
+#period2 = int(time.mktime(dateToday.timetuple()) #If Yes, set todays date
+#interval = input("Enter 1mo, 1d or 1m")
 interval = '1mo' # 1d, 1m
 
 
 query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+
 
 df = pd.read_csv (query_string, usecols=['Date', 'Close'])
 print(df)
