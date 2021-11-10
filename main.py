6589086161f72218a2fe9708a834cb9d6817312e
ksand97 <krisa18@uia.no>
2021-11-10 16:04:14 +0100

    Threading Request_api, Main and stl_generator_12p

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from stl_generation import stl_generator_12p
-import api_info
+import request_api
 
 
-y=api_info.Y
+y=request_api.Y
 
-y1=10e-3*y[1]
-y2=10e-3*y[2]
-y3=10e-3*y[3]
-y4=10e-3*y[4]
-y5=10e-3*y[5]
-y6=10e-3*y[6]
-y7=10e-3*y[7]
-y8=10e-3*y[8]
-y9=10e-3*y[9]
-y10=10e-3*y[10]
-y11=10e-3*y[11]
-y12=10e-3*y[12]
+# index 0??
+y1=10e-3*y[0]
+y2=10e-3*y[1]
+y3=10e-3*y[2]
+y4=10e-3*y[3]
+y5=10e-3*y[4]
+y6=10e-3*y[5]
+y7=10e-3*y[6]
+y8=10e-3*y[7]
+y9=10e-3*y[8]
+y10=10e-3*y[9]
+y11=10e-3*y[10]
+y12=10e-3*y[11]
 
 
 if __name__ == "__main__":
--- request_api.py
+++ request_api.py
@@ -4,51 +4,60 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def request():
+    
 
-# Setter inn ønskene paramtere
-ticker = input("Shortname:" )
-#ticker = 'TSLA'
-print("Year: ")
-year = input()
-print("Mounth:" )
-mounth = input()
-print("Day: ")
-day = input()
-print("Hour: ")
-hour = input()
-print("Minutes: ")
-minutes = input()
+    # Setter inn ønskene paramtere
+    ticker = input("Shortname:" )
+    #ticker = 'TSLA'
+    print("Year: ")
+    year = input()
+    print("Mounth:" )
+    mounth = input()
+    print("Day: ")
+    day = input()
+    print("Hour: ")
+    hour = input()
+    print("Minutes: ")
+    minutes = input()
 
-#period1 = int(time.mktime(datetime.datetime(2020, 11, 1, 23, 59).timetuple()))
-period1 = int(time.mktime(datetime.datetime(int(year), int(mounth), int(day), int(hour), int(minutes)).timetuple()))
+    #period1 = int(time.mktime(datetime.datetime(2020, 11, 1, 23, 59).timetuple()))
+    period1 = int(time.mktime(datetime.datetime(int(year), int(mounth), int(day), int(hour), int(minutes)).timetuple()))
 
-print("Year end: ")
-year_end = input()
-print("Mounth end:" )
-mounth_end = input()
-print("Day end: ")
-day_end = input()
-print("Hour end: ")
-hour_end = input()
-print("Minutes end: ")
-minutes_end = input()
-period2 = int(time.mktime(datetime.datetime(int(year_end), int(mounth_end), int(day_end), int(hour_end), int(minutes_end)).timetuple()))
-#period2 = int(time.mktime(datetime.datetime(2021, 11, 1, 23, 59).timetuple()))
-#period2 = int(time.mktime(dateToday.timetuple()) #If Yes, set todays date
-#interval = input("Enter 1mo, 1d or 1m")
-interval = '1mo' # 1d, 1m
-
-
-query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+    print("Year end: ")
+    year_end = input()
+    print("Mounth end:" )
+    mounth_end = input()
+    print("Day end: ")
+    day_end = input()
+    print("Hour end: ")
+    hour_end = input()
+    print("Minutes end: ")
+    minutes_end = input()
+    period2 = int(time.mktime(datetime.datetime(int(year_end), int(mounth_end), int(day_end), int(hour_end), int(minutes_end)).timetuple()))
+    #period2 = int(time.mktime(datetime.datetime(2021, 11, 1, 23, 59).timetuple()))
+    #period2 = int(time.mktime(dateToday.timetuple()) #If Yes, set todays date
+    interval = input("Enter 1mo, 1d or 1m: ")
+    #interval = '1mo' # 1d, 1m
 
 
-df = pd.read_csv (query_string, usecols=['Date', 'Close'])
-print(df)
-df_close_as_np = df['Close'].to_numpy()
+    query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
 
-df_as_np = df.to_numpy()
-print(df_as_np)
-print((len(df_as_np)))
-x = np.arange(0, len(df_as_np))
-plt.plot(x, df_close_as_np)
-plt.show()
+
+    df = pd.read_csv (query_string, usecols=['Date', 'Close'])
+    print(df)
+    df_close_as_np = df['Close'].to_numpy()
+
+    df_as_np = df.to_numpy()
+    
+    Y = df_close_as_np
+    print(df_as_np)
+    print((len(df_as_np)))
+    x = np.arange(0, len(df_as_np))
+    plt.plot(x, df_close_as_np)
+    plt.show()
+    return (Y)
+
+
+
+Y = request()
